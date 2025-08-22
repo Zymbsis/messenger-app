3637313fcea8594aea23ba5ceed02706427c0cf5
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-22 16:30:46 +0200

    BE:feat(ws): add ws protection

--- backend/core/auth/dependencies.py
+++ backend/core/auth/dependencies.py
@@ -2,22 +2,29 @@ from typing import Annotated
 
 from core.auth.jwt import verify_token
 from core.db import SessionDependency
-from fastapi import Depends, HTTPException, Security, status
+from fastapi import Cookie, Depends, HTTPException, Security, status
 from fastapi.security import APIKeyCookie
 from models import User
 from repositories.user import UserRepository
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-auth_scheme = APIKeyCookie(name="access_token", auto_error=True)
-TokenDependency = Annotated[str, Security(auth_scheme)]
+http_auth_scheme = APIKeyCookie(name="access_token", auto_error=True)
+TokenDependency = Annotated[str, Depends(http_auth_scheme)]
+
+WSTokenDependency = Annotated[str | None, Cookie(alias="access_token")]
 
 
-async def get_current_user(
-    session: SessionDependency,
-    token: TokenDependency,
-) -> User:
+def verify_token_http(token: TokenDependency) -> str:
     payload = verify_token(token)
-    user_id = payload.sub
+    return payload.sub
 
+
+def verify_token_ws(token: WSTokenDependency) -> str:
+    payload = verify_token(token)
+    return payload.sub
+
+
+async def get_current_user(session: AsyncSession, user_id: str):
     repo = UserRepository(session)
     user = await repo.get_user_by_id(int(user_id))
     if not user:
@@ -27,4 +34,19 @@ async def get_current_user(
     return user
 
 
-CurrentUserDependency = Annotated[User, Depends(get_current_user)]
+async def get_current_user_http(
+    session: SessionDependency,
+    user_id: Annotated[str, Depends(verify_token_http)],
+) -> User:
+    return await get_current_user(session, int(user_id))
+
+
+async def get_current_user_ws(
+    session: SessionDependency,
+    user_id: Annotated[str, Depends(verify_token_ws)],
+) -> User:
+    return await get_current_user(session, int(user_id))
+
+
+CurrentUserDependency = Annotated[User, Security(get_current_user_http)]
+CurrentUserWSDependency = Annotated[User, Security(get_current_user_ws)]
