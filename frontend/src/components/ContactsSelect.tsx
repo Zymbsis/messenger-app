acb926a7575aff7153d806f0fb5727b3abcb71bc
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-23 17:22:50 +0200

    FE:feat(chat): add contacts select

---
+++ frontend/src/components/ContactsSelect.tsx
@@ -0,0 +1,41 @@
+import type { ChangeEvent } from 'react';
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { useNavigate } from 'react-router';
+import { createNewChat } from '../redux/chats/operations';
+import { selectUsers } from '../redux/users/selectors';
+import { IoMdPersonAdd } from 'react-icons/io';
+
+const ContactsSelect = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const users = useAppSelector(selectUsers);
+
+  const handleSelectUser = async ({
+    target: { value },
+  }: ChangeEvent<HTMLSelectElement>) => {
+    const chat = await dispatch(createNewChat(Number(value))).unwrap();
+    navigate(`/chats/${chat.id}`);
+  };
+
+  return (
+    <label
+      className='w-14 relative inline-flex h-full'
+      title='Start new chat'
+      id='users'>
+      <select
+        onChange={handleSelectUser}
+        className='h-full w-14 cursor-pointer opacity-0'
+        name='users'
+        id='users'>
+        {users.map((user) => (
+          <option key={user.id} value={user.id}>
+            {user.email}
+          </option>
+        ))}
+      </select>
+      <IoMdPersonAdd className='w-full h-full absolute top-0 right-0 pointer-events-none' />
+    </label>
+  );
+};
+
+export default ContactsSelect;
