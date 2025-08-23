51b474d4f519ce35f90376383825e7d4d169e33d
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-23 18:15:06 +0200

    BE:feat(auth): add base CORS

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from core.db import close_db, init_db
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, chats, users, websockets
 
 
@@ -14,6 +15,16 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(chats.router)
