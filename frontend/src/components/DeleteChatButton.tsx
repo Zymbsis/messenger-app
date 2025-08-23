9d8fb96f04ff88527808a2900e99d795979336e7
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-23 17:21:13 +0200

    FE:feat(chat): add delete chat button

---
+++ frontend/src/components/DeleteChatButton.tsx
@@ -0,0 +1,18 @@
+import { MdDeleteOutline } from 'react-icons/md';
+import { deleteChat } from '../redux/chats/operations';
+import { useAppDispatch } from '../redux/hooks';
+
+const DeleteChatButton = ({ chatId }: { chatId: number }) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <button
+      onClick={() => dispatch(deleteChat(chatId))}
+      className='h-full w-11 py-4 pr-3 pl-1'
+      type='button'>
+      <MdDeleteOutline className='w-full h-full' />
+    </button>
+  );
+};
+
+export default DeleteChatButton;
