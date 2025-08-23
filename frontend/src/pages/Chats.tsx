93f7ac113886f5b9a29c28c4bcae3416161ee011
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-23 18:13:02 +0200

    FE:feat(auth): add chats page

--- frontend/src/pages/Chats.tsx
+++ frontend/src/pages/Chats.tsx
@@ -1,7 +1,30 @@
 import { Outlet } from 'react-router';
+import { useAppDispatch } from '../redux/hooks';
+import { useEffect } from 'react';
+import { getAllChats } from '../redux/chats/operations';
+import { getAllUsers, getCurrentUser } from '../redux/users/operations';
+import ChatsHeader from '../components/ChatsHeader';
+import ChatsList from '../components/ChatsList';
 
 const Chats = () => {
-  return <Outlet />;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+    dispatch(getAllChats());
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
+  return (
+    <>
+      <ChatsHeader />
+      <main className='flex h-[calc(100%-56px)]'>
+        <ChatsList />
+        <ul></ul>
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default Chats;
