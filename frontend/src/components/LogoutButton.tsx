009e2a35576fe7ddfed9675c0f87aceb44e2f2b3
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-23 13:37:30 +0200

    FE:feat(auth): add logout button

---
+++ frontend/src/components/LogoutButton.tsx
@@ -0,0 +1,20 @@
+import { CiLogout } from 'react-icons/ci';
+import { useAppDispatch } from '../redux/hooks';
+import { logout } from '../redux/auth/operations';
+
+const LogoutButton = () => {
+  const dispatch = useAppDispatch();
+  const handleLogout = () => dispatch(logout());
+
+  return (
+    <button
+      className='size-14 p-3'
+      type='button'
+      title='Logout'
+      onClick={handleLogout}>
+      <CiLogout className='w-full h-full' />
+    </button>
+  );
+};
+
+export default LogoutButton;
