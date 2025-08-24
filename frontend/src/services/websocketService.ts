2aa22c52b0140ed5b6ef3f5a2ec481a824b9edd3
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2025-08-24 03:16:57 +0200

    FE:feat(websocket): add websocket service

---
+++ frontend/src/services/websocketService.ts
@@ -0,0 +1,45 @@
+import { toast } from 'sonner';
+
+import type { Message } from '../redux/messages/slice';
+import type { EventData } from '../redux/api/apiSlice';
+
+type UpdateMessagesFn = (updateRecipe: (draft: Message[]) => void) => void;
+
+export class WebSocketService extends WebSocket {
+  constructor(url: string, updateMessagesFn: UpdateMessagesFn) {
+    super(url);
+
+    this.onmessage = (event) => {
+      try {
+        const { type, payload: message } = JSON.parse(event.data) as EventData;
+
+        if (type === 'new_message') {
+          updateMessagesFn((draft) => {
+            draft.unshift(message);
+          });
+        }
+
+        if (type === 'edit_message') {
+          updateMessagesFn((draft) => {
+            const existing = draft.findIndex((msg) => msg.id === message.id);
+            if (existing !== -1) draft[existing] = message;
+          });
+        }
+
+        if (type === 'delete_message') {
+          updateMessagesFn((draft) =>
+            draft.filter((msg) => msg.id !== message.id),
+          );
+        }
+      } catch (error) {
+        toast.error('WebSocket: Error receiving new message');
+        console.error(error);
+      }
+    };
+
+    this.onerror = (error) => {
+      toast.error('WebSocket: Connection lost');
+      console.error(error);
+    };
+  }
+}
